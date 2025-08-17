4f61453eb84b7280b6eac3a079268f327b405657
Victorbr779 <victorbr7guimaraes79@gmail.com>
2025-08-17 23:53:51 +0000

    Jogo de Batalha Naval Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,91 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+void lin(){
+    printf("\n");
+}
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+int main(){
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    lin();
+    printf("\033[1;42m#### JOGO BATALHA NAVAL ####\033[m\n");
+    lin();
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // #### JOGO DE BATALHA NAVAL AVENTUREIRO ####
+
+    int tabuleiro [10][10] = {0};
+    char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    char coluna [10] = {1,2,3,4,5,6,7,8,9,10};
+
+  
+    // ESTRUTURA DA LINHA DO ALFABETO
+    printf("   ");
+    for (int i = 0; i < 10; i++){
+        printf("\033[1;41m%c \033[m", linha[i]);
+    }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // ESTRUTURA DA COLUNA DE 1 A 10 E A MATRIZ
+    lin();
+    for (int j = 0; j < 10; j++){
+        printf("\033[1;41m%2d \033[m", coluna[j]);
+        for (int i = 0; i < 10; i++){
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+            // Coloca o navio da diagonal direita
+            if (i == j){
+                tabuleiro[i][j] = 3;
+                printf("\033[1;44m%d \033[m", tabuleiro[i][j]);
+            
+            // Coloca o navio na diagonal esquerda
+            }else if (i + j == 9){
+                tabuleiro[i][j] = 3;
+                printf("\033[1;44m%d \033[m", tabuleiro[i][j]);
+            }else{
+                printf("\033[1;44m%d \033[m", tabuleiro[i][j]);
+            }
+
+
+            // Coloca 2 navios em diagonal de forma delimitada, formando então um "V"
+            int inicio_linha = 3, inicio_linha_2 = 7;
+            int inicio_coluna = 0, inicio_coluna_2 = 0;
+            int tamanho = 3, tamanho_2 = 3;
+            
+            // Estrutura da primeira diagonal 
+            for (int k = 0; k < tamanho; k++){
+                tabuleiro[inicio_linha + k][inicio_coluna + k] = 3;
+            }
+
+            // Estrutura da segunda diagonal
+            for (int l = 0; l < tamanho_2; l++){
+                tabuleiro[inicio_linha_2 - l][inicio_coluna_2 + l] = 3;
+
+            }
+
+            // Coloca um navio na vertical
+            int linha_vertical = 8;
+            int coluna_vertical = 3;
+            int tamanho_vertical = 3;
+
+            for (int v = 0; v < tamanho_vertical; v++){
+                tabuleiro[linha_vertical][coluna_vertical + v] = 3;
+            }
+
+            // Coloca o navio na horizontal
+
+            int linha_horizontal = 3; 
+            int coluna_horizontal = 8;
+            int tamanho_horizonte = 3;
+
+            for (int h = 0; h < tamanho_horizonte; h++){
+                tabuleiro[linha_horizontal + h][coluna_horizontal] = 3;
+            }            
+            
+        }
+        lin();
+    }
+
+    lin();
+    printf("\033[1;42m############################\033[m\n");
+    lin();
 
     return 0;
 }
